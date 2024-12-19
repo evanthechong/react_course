3561a0c35adcc781a635c0d1783d0916ea9717a8
echo0037 <echo0037@student.monash.edu>
2024-12-19 21:56:34 +0800

    first upload of all files

---
+++ src/App2.js
@@ -0,0 +1,17 @@
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
+export const App2 = () => {
+    return(
+        <>
+        <Header/>
+        <div>
+            <h1 classname="header">Hello</h1>
+        </div>
+        <Footer/>
+        </>
+);
+}
+
+
+
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.jpg"
+
+
+export const Header = () => {
+    return (
+      <div>Header
+        <img className="logo" src={logo}/>
+      </div>
+    )
+  }
+  
